c03fead0bfa46c169172f9cccba122361b19d143
Vishal Bhatia <124044416+Vishal-Kaku@users.noreply.github.com>
2023-07-29 17:55:34 +0530

    add two numbers
    adding numbers using c++

---
+++ Add_Two_Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the value of A = ";
+    cin>>a;
+    cout<<"Enter the value of B = ";
+    cin>>b;
+    {
+        c = a + b;
+    }
+    cout<<"Result is = "<<c;
+    return 0;
+}
